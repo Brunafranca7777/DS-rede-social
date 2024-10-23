fc18d38e79eb536221ee069b225d9af9792cdb47
Brunafranca7777 <142025900+Brunafranca7777@users.noreply.github.com>
2024-10-23 08:04:13 -0300

    aula10

--- graficos/common.js
+++ graficos/common.js
@@ -3,3 +3,15 @@ const getCSS = (variavel) => {
 };
 
 export { getCSS }
+
+const getCSS = (variavel) => {
+  return getComputedStyle(document.body).getPropertyValue(variavel)
+}
+
+const tickConfig = {
+  color: getCSS('--primary-color'),
+  size: 16,
+  family: getCSS('--font')
+}
+
+export { getCSS, tickConfig }
